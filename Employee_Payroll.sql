159e5ad11c590f07b453599ad3bed203d37612e9
Rahulgautama <rahul8874950727@gmail.com>
2023-06-08 00:43:49 +0530

    Create Database using ado .net

--- Employee_Payroll.sql
+++
@@ -1,5 +0,0 @@
-select * from employee_payroll inner join  Employee_Department on employee_payroll.ID=Employee_Department.DepartmentId where name='Bill'
-
-
-select * from employee_payroll inner join  Employee_Department on employee_payroll.ID=Employee_Department.DepartmentId WHERE StartDate BETWEEN CAST('2018-01-01'
-AS DATE) AND cast(GETDATE()as date);
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SqlAdo.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdoDotNet ado = new AdoDotNet();
+            ado.CreateDatabase();
+        }
+    }
+}
