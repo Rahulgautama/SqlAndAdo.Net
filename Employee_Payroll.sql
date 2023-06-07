496c457e82068704ef5da18d27303e8c767f7d62
Rahulgautama <rahul8874950727@gmail.com>
2023-06-07 23:17:56 +0530

    Extend the Employee_payroll and add BasicPay,Deduction,TaxablePay,IncomeTax,NetPay

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -1,2 +1,1 @@
-alter table employee_payroll add Phone int,Address varchar(100) default 'Lucknow' not null,Department varchar(50) default 'FinanceDepartment' not null;
-
+alter table employee_payroll add BasicPay decimal,Deductions decimal, TaxablePay decimal,IncomeTax decimal, NetPay decimal
