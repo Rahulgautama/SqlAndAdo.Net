a9dd30b63886e5e1653da79e8df1e0a02eec49df
Rahulgautama <rahul8874950727@gmail.com>
2023-06-08 00:29:58 +0530

    many to many and select data of all codition

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -1,7 +1,5 @@
-create Table Employee_Department(Id int primary key IDENTITY(1,1),EmployeeId int ,DepartmentId int,FOREIGN KEY (DepartmentId) REFERENCES employee_payroll(Id))
+select * from employee_payroll inner join  Employee_Department on employee_payroll.ID=Employee_Department.DepartmentId where name='Bill'
 
 
-select * from employee_payroll inner join  Employee_Department on employee_payroll.ID=Employee_Department.DepartmentId;
-
-
-
+select * from employee_payroll inner join  Employee_Department on employee_payroll.ID=Employee_Department.DepartmentId WHERE StartDate BETWEEN CAST('2018-01-01'
+AS DATE) AND cast(GETDATE()as date);
