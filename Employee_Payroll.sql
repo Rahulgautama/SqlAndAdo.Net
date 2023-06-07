4d85b0ba2864ff7424cb0d459ed376b4ab18873f
Rahulgautama <rahul8874950727@gmail.com>
2023-06-07 23:12:04 +0530

    Extend the Employee_payroll and add phone address not null

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -1,12 +1,2 @@
-select sum(salary) as TotalSum from employee_payroll where Gender='M'
-
-select Avg(salary) as TotalSum from employee_payroll where Gender='F'
-
-select Min(salary) as TotalSum from employee_payroll where Gender='F'
-
-select Max(salary) as TotalSum from employee_payroll where Gender='M'
-
-select Count(salary) as TotalSum from employee_payroll where Gender='F'
-
-SELECT SUM(salary) FROM employee_payroll WHERE Gender = 'M' GROUP BY Gender;
+alter table employee_payroll add Phone int,Address varchar(100) default 'Lucknow' not null,Department varchar(50) default 'FinanceDepartment' not null;
 
