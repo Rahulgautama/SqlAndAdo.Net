cf516d0cbece079aa98ba509d9f45cd00db5c9dd
Rahulgautama <rahul8874950727@gmail.com>
2023-06-08 00:05:40 +0530

    Create Table EmployeeDepartment and add column UC7

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -1,2 +1,2 @@
-create Table Employee_Department(Id int primary key IDENTITY(1,1),DepartmentId int,DepartmentName varchar,Salary decimal,StartDate date,Gender char(1))
+create Table Employee_Department(Id int primary key IDENTITY(1,1),DepartmentId int,DepartmentName varchar(100),Salary decimal,StartDate date,Gender char(1))
 
