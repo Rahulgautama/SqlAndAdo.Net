8da12801c352cfcda0ab7f8e850aa4a540d788b0
Rahulgautama <rahul8874950727@gmail.com>
2023-06-08 00:23:12 +0530

    Create Table EmployeeDepartment and add column UC7

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -1,2 +1,7 @@
-create Table Employee_Department(Id int primary key IDENTITY(1,1),DepartmentId int,DepartmentName varchar(100),Salary decimal,StartDate date,Gender char(1))
+create Table Employee_Department(Id int primary key IDENTITY(1,1),EmployeeId int ,DepartmentId int,FOREIGN KEY (DepartmentId) REFERENCES employee_payroll(Id))
+
+
+select * from employee_payroll inner join  Employee_Department on employee_payroll.ID=Employee_Department.DepartmentId;
+
+
 
