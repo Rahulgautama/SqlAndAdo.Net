407b4f72cb031a3809da8cfd23fdc544e914ab7e
Rahulgautama <rahul8874950727@gmail.com>
2023-06-07 22:23:09 +0530

    Using MSSQL Crud Operation

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -1,3 +1,5 @@
+Insert Into employee_payroll(Name,Salary,StartDate) values('John',2738,'2023-06-04')
+
 Select * from employee_payroll
 
 update employee_payroll set Name='Niteesh',Salary=1883 where Id=2
