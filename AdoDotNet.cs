84a43986ae699bf1c7438675c0fa50423381bc41
Rahulgautama <rahul8874950727@gmail.com>
2023-06-08 01:23:25 +0530

    Select Data sum,avg,min,max,count function and group by using ado .net

--- AdoDotNet.cs
+++ AdoDotNet.cs
@@ -12,22 +12,53 @@ namespace SqlAdo.Net
             try
             {
                 string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;database=payroll_service; Integrated Security=True";
-                string query = @"Select * from employee_payroll where Salary>1000 and Salary<2500";
+                string query = @"Select Sum(Salary) as TotalSum,Avg(Salary) As Average,Min(Salary) as MinSalary,Count(Salary)as CountSalary,Max(Salary)as MaxSalary from employee_payroll where Gender='F'";
                 SqlConnection connetion = new SqlConnection(connectionString);
                 connetion.Open();
 
                 SqlCommand command = new SqlCommand(query, connetion);
                 SqlDataReader reader = command.ExecuteReader();
 
-                Console.WriteLine("Id , Name," + "  Salary , Date");
+                Console.WriteLine("TotalSum , Average, MinSalary, CountSalary, MaxSalary");
                 Console.WriteLine("=============================");
 
                 while (reader.Read())
                 {
-                    Console.Write(reader["Id"].ToString() + ", ");
-                    Console.Write(reader["Name"].ToString() + ", ");
-                    Console.Write(reader["Salary"].ToString() + ", ");
-                    Console.WriteLine(reader["StartDate"].ToString() + ", ");
+                   
+                    Console.Write(reader["TotalSum"].ToString() + ", ");
+                    Console.Write(reader["Average"].ToString() + ", ");
+                    Console.Write(reader["MinSalary"].ToString() + ", ");
+                    Console.Write(reader["CountSalary"].ToString() + ", ");
+                    Console.Write(reader["MaxSalary"].ToString() + ", ");                    
+                }
+                reader.Close();
+                connetion.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        public void SelectGroupBy()
+        {
+            try
+            {
+                string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;database=payroll_service; Integrated Security=True";
+                string query = @"SELECT SUM(salary) as TotalSum FROM employee_payroll WHERE gender = 'F' GROUP BY gender;";
+                SqlConnection connetion = new SqlConnection(connectionString);
+                connetion.Open();
+
+                SqlCommand command = new SqlCommand(query, connetion);
+                SqlDataReader reader = command.ExecuteReader();
+
+                Console.WriteLine("TotalSum");
+                Console.WriteLine("=============================");
+
+                while (reader.Read())
+                {
+
+                    Console.Write(reader["TotalSum"].ToString());
+                  
                 }
                 reader.Close();
                 connetion.Close();
