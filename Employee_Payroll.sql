75fd48fb1062be634fe2ff014091983ca4f0a8b5
Rahulgautama <111899129+Rahulgautama@users.noreply.github.com>
2023-06-07 22:16:30 +0530

    Update Employee_Payroll.sql

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -1,2 +1,2 @@
-CREATE TABLE dbo.employee_payroll(ID int primary key IDENTITY(1,1) NOT NULL,Name varchar(50) NULL,Salary int NULL,StartDate date NULL)
+CREATE TABLE dbo.employee_payroll(ID int primary key IDENTITY(1,1) NOT NULL,Name varchar(50) NULL,Salary decimal NULL,StartDate date NULL)
 
